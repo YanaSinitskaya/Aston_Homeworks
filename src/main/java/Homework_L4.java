64385bd9114b05f4d44a0bb9388443db893f3aeb
Yana Sinitskaya <y.sinitskaya11@gmail.com>
2024-04-21 21:04:38 +0300

    Tasks from homework L4 Added Homework_L4 class Refresh Main class

---
+++ src/main/java/Homework_L4.java
@@ -0,0 +1,128 @@
+import java.util.Arrays;
+
+public class Homework_L4 {
+
+    public void printThreeWords() {
+        System.out.println("Orange\nBanana\nApple");
+    }
+
+    public void checkSumSign() {
+        int a = 10;
+        int b = -45;
+        int c = a + b;
+        if (c >= 0) {
+            System.out.println("Сумма положительная");
+
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public void printColor() {
+        int value = 5;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public void compareNumbers() {
+        int a = 88;
+        int b = 399;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
+    public boolean checkSumRange(int a, int b) {
+        int sum = a + b;
+        if (sum >= 10 && sum <= 20) {
+            return true;
+        }
+        return false;
+    }
+
+    public String checkPositiveOrNegativeString(int a) {
+        if (a >= 0) {
+            return "Положительное";
+        }
+        return "Отрицательное";
+    }
+
+    public boolean checkPositiveOrNegativeBoolean(int a) {
+        if (a >= 0) {
+            return false;
+        }
+        return true;
+    }
+
+    public String writePhraseNTimes(String phrase, int quantity) {
+        String repeat = phrase.repeat(quantity);
+        return repeat;
+    }
+
+    public boolean isLeapYear(int year) {
+        if (year % 4 == 0 || year % 400 == 0 && year % 100 != 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public void rewriteArray() {
+        int[] array = new int[]{1, 0, 0, 1, 1, 0, 0, 1};
+        System.out.println("Массив задание 10 " + Arrays.toString(array));
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                array[i] = 1;
+            } else if (array[i] == 1) {
+                array[i] = 0;
+            }
+        }
+        System.out.println("Преобразован в массив " + Arrays.toString(array));
+    }
+
+    public void fillArray() {
+        int[] array = new int[100];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
+        System.out.println("Массив задание 11 " + Arrays.toString(array));
+    }
+
+    public void multiplyNumbersLessThan6() {
+        int[] array = new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        System.out.println("Массив задание 12 " + Arrays.toString(array));
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 6) {
+                array[i] *= 2;
+            }
+        }
+        System.out.println("Преобразован в массив " + Arrays.toString(array));
+    }
+
+    public void fillTwoDimensionalArray() {
+        System.out.println("Массив задание 13 ");
+        int[][] array = new int[4][4];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                array[i][i] = 1;
+                System.out.print(array[i][j] + "");
+            }
+            System.out.println();
+        }
+    }
+
+    public void lenArray(int len, int initialValue) {
+        int[] array = new int[len];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = initialValue;
+        }
+        System.out.println("Массив задание 14 " + Arrays.toString(array));
+    }
+}
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-
+        Homework_L4 homework = new Homework_L4();
+        homework.printThreeWords();
+        homework.checkSumSign();
+        homework.printColor();
+        homework.compareNumbers();
+        System.out.println(homework.checkSumRange(44,972));
+        System.out.println(homework.checkPositiveOrNegativeString(0));
+        System.out.println(homework.checkPositiveOrNegativeBoolean(-1557));
+        System.out.println(homework.writePhraseNTimes("Hello\n", 4));
+        System.out.println(homework.isLeapYear(2024));
+        homework.rewriteArray();
+        homework.fillArray();
+        homework.multiplyNumbersLessThan6();
+        homework.fillTwoDimensionalArray();
+        homework.lenArray(5, 1);
     }
 }
