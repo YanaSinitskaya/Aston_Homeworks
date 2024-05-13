9aa5880511e0c15517ca45c69d54b83e55723f9a
Yana Sinitskaya <y.sinitskaya11@gmail.com>
2024-05-13 18:02:14 +0300

    added task from lesson 8 homework

---
+++ src/main/java/lesson8/Lesson8_Main.java
@@ -0,0 +1,30 @@
+package lesson8;
+
+public class Lesson8_Main {
+    public static void main(String[] args) {
+        String[][] array = {{"a", "b", "c", "d"}, {"1", "2", "3", "4"}, {"e", "f", "g", "h"}, {"5", "6", "7", "8"}};
+        try {
+            sumArray(array);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public static int sumArray(String array[][]) throws MyArraySizeException, MyArrayDataException {
+        int sum = 0;
+        if (array.length > 4 || array[0].length > 4) {
+            throw new MyArraySizeException("Размер массива больше 4-ех");
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    sum = sum + Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Неподходящий элемент массива для суммирования в строке " + i + " и столбце " + j);
+                }
+            }
+        }
+        return sum;
+    }
+}
---
+++ src/main/java/lesson8/MyArraySizeException.java
@@ -0,0 +1,7 @@
+package lesson8;
+
+public class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
