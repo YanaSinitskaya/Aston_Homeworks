192b7fa36d39607be9ef2bf08e485136a49d3dad
Yana Sinitskaya <y.sinitskaya11@gmail.com>
2024-06-04 21:30:52 +0300

    Fixed comments

--- src/main/java/lesson8/Lesson8_Main.java
+++ src/main/java/lesson8/Lesson8_Main.java
@@ -2,20 +2,18 @@ package lesson8;
 
 public class Lesson8_Main {
     public static void main(String[] args) {
-        String[][] array = {{"a", "b", "c", "d"}, {"1", "2", "3", "4"}, {"e", "f", "g", "h"}, {"5", "6", "7", "8"}};
+        String[][] array = {{"a", "b", "c", "d"}, {"1", "2", "3" }, {"e", "f", "g", "h"}, {"5", "6", "7", "8"}};
         try {
             sumArray(array);
         } catch (MyArraySizeException | MyArrayDataException e) {
-            System.out.println(e.getMessage());
+            System.err.println(e.getMessage());
         }
 
     }
 
     public static void sumArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
         int sum = 0;
-        if (array.length > 4 || array[0].length > 4) {
-            throw new MyArraySizeException("Размер массива больше 4-ех");
-        }
+        checkSizeOfArray(array);
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 try {
@@ -26,5 +24,16 @@ public class Lesson8_Main {
             }
         }
         System.out.println(sum);
+    }
+
+    private static void checkSizeOfArray(String[][] array) throws  MyArraySizeException {
+        if (array.length != 4) {
+            throw new MyArraySizeException("Размер массива не равен 4х4");
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length != 4) {
+                throw new MyArraySizeException("Размер массива не равен 4х4");
+            }
+        }
     }
 }
